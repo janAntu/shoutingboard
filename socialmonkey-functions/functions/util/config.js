c715d4fbc7e0995954ef09ac6c8814e280b5ac2d
janAntu <randrus@ucsd.edu>
2019-09-22 12:59:21 -0700

    Initialized React app

---
+++ socialmonkey-functions/functions/util/config.js
@@ -0,0 +1,9 @@
+module.exports = config = {
+    apiKey: "AIzaSyCB_2RIOEiOsmFcpjaGy0fDesRmnxd3GxE",
+    authDomain: "socialmonkey-eb10e.firebaseapp.com",
+    databaseURL: "https://socialmonkey-eb10e.firebaseio.com",
+    projectId: "socialmonkey-eb10e",
+    storageBucket: "socialmonkey-eb10e.appspot.com",
+    messagingSenderId: "967941048956",
+    appId: "1:967941048956:web:1faad5201ae5e2246a17f9"
+};
